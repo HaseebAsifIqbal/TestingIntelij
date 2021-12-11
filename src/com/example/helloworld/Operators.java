05a8da67deafbcbdb9da178687df93bd93dbcb9f
HaseebAsifIqbal <haifromny@aol.com>
2021-12-11 13:35:59 -0500

    Operators Test

---
+++ src/com/example/helloworld/Operators.java
@@ -0,0 +1,22 @@
+package com.example.helloworld;
+
+public class Operators {
+
+    public static void main(String [] args){
+
+    double friends = 10;
+    friends /=  3;
+
+    int friendsAdd = 10;
+    friendsAdd++;
+
+    int friendsSubtract = 10;
+    friendsSubtract--;
+
+
+    System.out.println(friends);
+    System.out.println(friendsSubtract);
+    System.out.println(friendsAdd);
+
+    }
+}
