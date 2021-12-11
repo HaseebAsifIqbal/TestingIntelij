4a909215c33f893e95c1b1ef95925d20d9c11b4e
HaseebAsifIqbal <haifromny@aol.com>
2021-12-11 17:51:30 -0500

    For Loops Tutorial

---
+++ src/com/example/helloworld/ForLoop.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        for (int i = 10; i>=0; i--){
+
+            System.out.println(i);
+
+
+        }
+        System.out.println("Happy new year!");
+
+    }
+
+
+}
