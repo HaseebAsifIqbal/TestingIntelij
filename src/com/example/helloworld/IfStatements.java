8af500f9a5e6d3f18baf17bb5e00dd57c62de613
HaseebAsifIqbal <haifromny@aol.com>
2021-12-11 16:05:27 -0500

    If Statement tutorial test

---
+++ src/com/example/helloworld/IfStatements.java
@@ -0,0 +1,19 @@
+package com.example.helloworld;
+
+public class IfStatements {
+
+    public static void main(String[] args) {
+        int age = 18;
+
+        if (age >= 18) {
+
+            System.out.println("You are an adult!");
+
+        }else{
+            System.out.println("You are not an adult!");
+        }
+
+
+    }
+
+}
