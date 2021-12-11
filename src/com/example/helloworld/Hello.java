cfe69cdcf2b294cbab432319ad87007f03e70e1e
HaseebAsifIqbal <haifromny@aol.com>
2021-12-11 11:53:39 -0500

    Commit Testing

---
+++ src/com/example/helloworld/Hello.java
@@ -0,0 +1,27 @@
+package com.example.helloworld;
+import java.util.Scanner;
+
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("What is your age? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("You are: "+age+" years old.");
+        System.out.println("What is your favorite food? ");
+        String food = scanner.nextLine();
+        System.out.println("Your favorite food is: "+food);
+
+
+        System.out.println("Hello "+name);
+
+    }
+
+    }
+
+
