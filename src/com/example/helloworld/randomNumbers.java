d1ad0dc340776d536abdaa0cf3cf3f417f3062bb
HaseebAsifIqbal <haifromny@aol.com>
2021-12-11 15:57:19 -0500

    RandomNumbers tutorial test

--- src/com/example/helloworld/randomNumbers.java
+++ src/com/example/helloworld/randomNumbers.java
@@ -1,11 +1,17 @@
 package com.example.helloworld;
 import java.util.Random;
-// Testing randomNumber and wathcing tutorial
+
 public class randomNumbers {
 
     public static void main(String[] args){
 
+        Random random = new Random();
 
+//        int x = random.nextInt(6)+1;
+//        double y = random.nextDouble();
+        boolean z = random.nextBoolean();
+
+        System.out.println(z);
 
 
 
